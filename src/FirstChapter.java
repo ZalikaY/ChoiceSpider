53b9e2719f0bfd178dfa7b62e439fde87010e631
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-07 18:28:42 -0400

    ChoiceSpider

---
+++ src/FirstChapter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FirstChapter {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		//Welcome Message, Player's name
+		System.out.println("Welcome,What is your name?");
+		
+		//Space
+		System.out.println();
+		
+		//
+	
+		
+		
+	}
+
+}
